28e578ae9464c07c34ba00378f6893796cdba82b
cedricporter@gmail.com <cedricporter@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-09-08 05:26:34 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@2 81c3a017-244d-672b-1dc6-c8d6962b81c2

---
+++ OpenGL-Scut/OpenGL-Scut/main.cpp
@@ -0,0 +1,28 @@
+//#include "gl/glut.h"
+//
+//void myDisplay(void)
+//{
+//	glClear(GL_COLOR_BUFFER_BIT);
+//	glRectf(-0.5f, -0.5f, 0.5f, 0.5f);
+//	glFlush();
+//}
+//int main(int argc, char *argv[])
+//{
+//	glutInit(&argc, argv);
+//	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+//	glutInitWindowPosition(100, 100);
+//	glutInitWindowSize(400, 400);
+//	glutCreateWindow("第一个OpenGL程序");
+//	glutDisplayFunc(&myDisplay);
+//	glutMainLoop();
+//	return 0;
+//}
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "EE";
+
+	return 0;
+}
