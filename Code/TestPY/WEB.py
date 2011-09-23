97c9603ce177867bb838a57f1d14ea63f56fb666
cedricporter@gmail.com <cedricporter@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-09-23 20:20:18 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@72 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/TestPY/WEB.py
+++ Code/TestPY/WEB.py
@@ -1,18 +1,69 @@
 import socket
-import emb
+import thread
+import xml.etree.ElementTree as ET
 
-host = ''
-port = 54321
+theID = 0
+Lock = thread.allocate_lock()
+dictLock = thread.allocate_lock()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+posDic = dict()
 
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+def GetAllInfo():
+    global posDic
 
-s.bind((host, port))
+    dictLock.acquire()
+    allpos = "<all>"
+    for node in posDic:
+        allpos += posDic[ node ]
+    allpos += "</all>"
+    dictLock.release()
 
-s.listen(5)
+    return allpos
+
+
+def Handler(sock):
+    global theID, posDic
+    global allpos, posLock
+
+    Lock.acquire()    
+    theID += 1
+    tempId = theID
+    Lock.release()
+
+    sock.send(str(tempId))
+    ##
+    
+    data = ''
+    while data != 'end':
+        info = GetAllInfo()
+        print info
+        sock.send(info)
+        user = sock.recv( 1024 )
+        
+        dictLock.acquire()
+        posDic[ tempId ] = user
+        dictLock.release()        
+        
+    print end
+        
+
+HOST = ''
+PORT = 1000
+
+s = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+s.bind((HOST, PORT))
+s.listen( 50 )
+
+id = 1
 
 while 1:
-    client_sock, client_addr = s.accept()
-    emb.hello( "Got " + str(client_sock.getpeername()) )
-    client_sock.close()
+    sock, addr = s.accept()
+
+    print sock.getpeername()
+
+    thread.start_new_thread( Handler, (sock,) )
+
+
+    
+    
+    
---
+++ Code/TestPY/client.py
@@ -0,0 +1,83 @@
+﻿import socket
+import thread
+
+x = 0.0
+y = 0.0
+z = 0.0
+
+posLock = thread.allocate_lock()
+goLock = thread.allocate_lock()
+
+host = '127.0.0.1'
+port = 1000
+BUF_SIZE = 1024
+
+theID = 0
+sock = None
+
+def mailLoop(lock):
+    global x, y, z, posLock
+    global host, port, BUF_SIZE
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+
+    # 返回自己的id
+    id = s.recv( BUF_SIZE )
+    while 1:        
+        allpos = s.recv(1024)
+        pos = "<user id=" + id + ">\n"
+        posLock.acquire()
+        pos += "<pos x=" + str(x) + " y=" + str(y) + " z=" + str(z) + "/>"
+        posLock.release()
+        pos += "\n</user>\n"
+        s.send(pos)
+        goLock.acquire()
+
+    lock.release()
+
+def SetPos(newx, newy, newz):
+    global x, y, z, posLock
+    posLock.acquire()
+    x, y, z = newx, newy, newz
+    posLock.release()
+    #goLock.release()
+
+def Connect():
+    global host, port, BUF_SIZE, sock, theID
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    theID = sock.recv( BUF_SIZE )
+    return theID
+    
+def Run():
+    global sock, theID
+    allpos = sock.recv(1024)
+    return allpos
+
+def Send():
+    global sock, theID
+    pos = "<user id=" + theID + ">"
+    posLock.acquire()
+    pos += "<pos x=" + str(x) + " y=" + str(y) + " z=" + str(z) + "/>"
+    posLock.release()
+    pos += "</user>"
+    sock.send(pos)
+    
+    
+
+
+#if __name__=="__main__":
+#   locks = []
+#    
+#    lock = thread.allocate_lock()
+#    lock.acquire()
+#    locks.append(lock)
+#    thread.start_new_thread( mailLoop, (lock, ) )
+#    
+#    for i in range( len(locks) ):
+#        while locks[ i ].locked() : pass
+    
+    
+    
+        
