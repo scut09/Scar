d7df4c45c819d947e76a8a894333d1588b63a690
cedricporter@gmail.com <cedricporter@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-09-24 03:13:05 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@73 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/TestPY/main.cpp
+++ Code/TestPY/main.cpp
@@ -1,22 +1,12 @@
 #include <boost/python.hpp>  
+//#include <boost/thread.hpp>
 #include <iostream>  
 #include <string>
 
 #include <Windows.h>
+#include <cstdlib>
 
 using namespace boost::python;  
-
-DWORD WINAPI ThreadFUnc( PVOID param )
-{
-	object main = *(object*)param;
-
-	//std::cout << extract<int>(main["port"]) ;
-
-	
-	
-
-	return 0;
-}
 
 void hello( char * msg )  
 {  
@@ -28,10 +18,50 @@ BOOST_PYTHON_MODULE(emb)
 	def("hello", hello);  
 }  
 
-int main()  
-{  
-	Py_Initialize();  
 
+
+class PyThreadStateLock 
+{
+public:  
+	PyThreadStateLock(void)   
+	{     
+		state = PyGILState_Ensure( );  
+	}    
+	~PyThreadStateLock(void)   
+	{ 
+		PyGILState_Release( state );  
+	} 
+private: 
+	PyGILState_STATE state; 
+}; 
+
+int x = rand();
+int y = rand();
+int z = rand();
+
+CRITICAL_SECTION lock;
+
+
+DWORD WINAPI ThreadFUnc( PVOID param )
+{
+	while ( 1 )
+	{
+		int tx, ty, tz;
+		std::cin >> tx >> ty >> tz;
+		EnterCriticalSection( &lock );
+		x = tx;
+		y = ty;
+		z = tz;
+		LeaveCriticalSection( &lock );
+	}
+
+	return 0;
+}
+
+
+
+void Run()
+{
 	try  
 	{  
 		initemb();  
@@ -42,12 +72,6 @@ int main()
 
 		PyObject * pModule = NULL; //声明变量   
 		PyObject * pFunc = NULL;       //声明变量  
-		//pModule = PyImport_ImportModule("client");  //这里是要调用的文件名  
-		//pFunc = PyObject_GetAttrString(pModule, "Connect"); //这里是要调用的函数名  
-		//PyEval_CallObject(pFunc, NULL);            //调用函数  
-
-		//PyObject * pRun = PyObject_GetAttrString(pModule, "Run");
-		//PyObject * pSend = PyObject_GetAttrString(pModule, "Send");
 
 		object main = import("client");
 		object id = main.attr("Connect")();
@@ -56,40 +80,30 @@ int main()
 		object Send = main.attr("Send");
 		object SetPos = main.attr("SetPos");
 
-		//PyRun_SimpleString( "Connect()" );
-
 		std::cout << extract<char*>(id) << std::endl;
 
 		while ( 1 )
 		{
-			std::string input;
-			std::cin >> input;
-			if ( input == "end" )
-				break;
+			//std::string input;
+			//std::cin >> input;
+			//if ( input == "end" )
+			//	break;
+
+			int tx, ty, tz;
+			EnterCriticalSection( &lock );
+			tx = x;
+			ty = y;
+			tz = z;
+			LeaveCriticalSection( &lock );
 
 			Run();
 			Send();
-			SetPos( 1, 2, 3 );
+			SetPos( tx, ty, tz );
 
-			//PyRun_SimpleString( "setPos( 1.1, 2.2, 3.3)" );
-			//PyEval_CallObject( pRun, NULL);            //调用函数  
-			//PyEval_CallObject( pSend, NULL);            //调用函数  
+			Sleep( 10 );
 		}	
 
 
-
-		//DWORD dwID;
-		//HANDLE handle = CreateThread( NULL, 0, ThreadFUnc, (PVOID)&main_namespace, 0, &dwID );	
-
-		//if ( handle == 0 )
-		//{
-		//	std::cout << "Failed";
-		//}
-
-		//Sleep( 1000 );
-		//exec_file("client.py", main_namespace );  
-
-		//WaitForSingleObject( handle, 0 );
 
 	}  
 	catch(...)  
@@ -99,7 +113,68 @@ int main()
 
 	std::cout << "ET";
 
-	Py_Finalize();
 
+}
+
+
+
+
+
+
+//boost::mutex m_io_monitor;
+//
+//
+//
+//void do_task()
+//{
+//	for (int i = 0; i < 10; ++i)
+//	{
+//		{
+//			boost::mutex::scoped_lock lock(m_io_monitor);
+//			PyRun_SimpleString("from time import time,ctime/nprint 'Today is',ctime(time())/n");
+//		}
+//
+//		boost::xtime xt;
+//		boost::xtime_get(&xt, boost::TIME_UTC); // initialize xt with current time
+//		xt.sec += 1; // change xt to next second
+//		boost::thread::sleep(xt); // do sleep
+//	}
+//}
+
+void SayHi()
+{
+	std::cout << "Hi";
+}
+
+
+int main()  
+{  
+	Py_Initialize();  
+	PyEval_InitThreads(); 
+	//PyEval_ReleaseThread(PyThreadState_Get()); 
+
+	//boost::thread func(SayHi);
+	//func.join();
+
+	InitializeCriticalSection( &lock );
+
+
+	DWORD dwID;
+	HANDLE handle = CreateThread( NULL, 0, ThreadFUnc, (PVOID)0, 0, &dwID );	
+
+	////if ( handle == 0 )
+	////{
+	////	std::cout << "Failed";
+	////}
+
+	////Sleep( 1000 );
+	////exec_file("client.py", main_namespace );  
+
+	WaitForSingleObject( handle, 0 );
+
+	Run();
+
+	PyGILState_Ensure(); 
+	Py_Finalize();
 	return 0;  
 }  
