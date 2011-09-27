ae8288791468bffbe868f338771dce4d7009341b
cedricporter@gmail.com <cedricporter@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-09-27 07:24:11 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@106 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/Scar/Scar/IServer.h
+++ Code/Scar/Scar/IServer.h
@@ -2,16 +2,18 @@
 创建时间: 2011-9-27   15:00
 文件名:   IServer.h
 作者:     华亮 Cedric Porter [ Stupid ET ]	
-说明:     
+说明:     服务器接口。服务器端应该继承IServer并实现IServer的接口，
+		系统其他部分针对IServer接口编程
 
 *********************************************************************/
 
 #ifndef _ISERVER_H
 #define _ISERVER_H
 
+#include <string>
+
 namespace Network
 {
-
 
 	/*
 	** 名字：IServer
