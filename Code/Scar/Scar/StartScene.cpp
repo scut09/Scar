85d41d9c0e96ae331a03cdc1754a6e19bb8fb178
kid.shusang@gmail.com <kid.shusang@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-10-11 13:11:27 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@145 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/Scar/Scar/StartScene.cpp
+++ Code/Scar/Scar/StartScene.cpp
@@ -3,20 +3,20 @@
 
 void StartScene::Run() 
 {
-	MyIrrlichtEngine* pEngine = MyIrrlichtEngine::GetEngine();
+	
 
-	irr::video::IVideoDriver* driver = pEngine->GetVideoDriver();
-
-	if ( count++ > 5000 )
+	/*if ( count++ > 5000 )
 	{
 		pEngine->currentScene = multiplayerScene;
 		Release();
 		pEngine->currentScene->Init();
-	}
+	}*/
 }
 
 void StartScene::Init() 
 {
+	pEngine = MyIrrlichtEngine::GetEngine();
+	driver = pEngine->GetVideoDriver();
 }
 
 void StartScene::Release() 
--- Code/Scar/Scar/StartScene.h
+++ Code/Scar/Scar/StartScene.h
@@ -6,7 +6,8 @@
 class StartScene : public GameScene
 {
 	int count;
-
+	MyIrrlichtEngine* pEngine;
+	irr::video::IVideoDriver* driver;
 
 public:
 	StartScene() : count( 0 )
