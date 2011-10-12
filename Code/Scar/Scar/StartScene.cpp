8d814adf8164c12d311c17e5baa3cdcc8b1fda20
kid.shusang@gmail.com <kid.shusang@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-10-12 08:50:07 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@156 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/Scar/Scar/StartScene.cpp
+++ Code/Scar/Scar/StartScene.cpp
@@ -1,15 +1,11 @@
 #include "StartScene.h"
 #include "def.h"
 
-UIImage u( MyIrrlichtEngine::GetEngine()->GetVideoDriver(), vector2d<s32>(100,100), 200, 200 );
-ITexture * gImg = MyIrrlichtEngine::GetEngine()->GetVideoDriver()->getTexture("D:\\f.jpg");
+
 
 
 void StartScene::Run() 
 {
-	//driver->beginScene(true, true, video::SColor(255,255,255,255));
-	//driver->endScene();
-	//u.Draw();
 	if ( count++ > 200 )
 	{
 		count = 0;
@@ -40,7 +36,6 @@ void StartScene::Release()
 void StartScene::Draw()
 {
 	driver->draw2DImage(gImg, vector2d<s32>(0,0));
-
 }
 
 
--- Code/Scar/Scar/StartScene.h
+++ Code/Scar/Scar/StartScene.h
@@ -10,9 +10,14 @@ class StartScene : public GameScene
 	MyIrrlichtEngine* pEngine;
 	irr::video::IVideoDriver* driver;
 
+	UIImage u;
+	ITexture * gImg;
+
 public:
-	StartScene() : count( 0 )
-	{}
+	StartScene() : count( 0 ), u( MyIrrlichtEngine::GetEngine()->GetVideoDriver(), vector2d<s32>(100,100), 200, 200 )
+	{
+		gImg = MyIrrlichtEngine::GetEngine()->GetVideoDriver()->getTexture("D:\\f.jpg");
+	}
 
 	shared_ptr<GameScene> multiplayerScene;
 
