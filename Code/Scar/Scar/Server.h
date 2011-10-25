74a9e82e6bca13128e3b297fe562d5a56227c7a5
streamYcx@gmail.com <streamYcx@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-10-25 04:06:45 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@329 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/Scar/Scar/Server.h
+++ Code/Scar/Scar/Server.h
@@ -35,18 +35,30 @@ namespace Network
 			while ( 1 )
 			{
 				ip::udp::endpoint ep;
-				system::error_code ec;
+				//system::error_code ec;
 				
-				sock.receive_from( buffer( buf ), ep, 0, ec );
+				//sock.receive_from( buffer( buf ), ep, 0, ec );
 
-				if ( ec && ec != error::message_size )
-				{
-					// error
-				}
+				//if ( ec && ec != error::message_size )
+				//{
+				//	// error
+				//}
 
-				ep.address();
+				//ep.address();
 
-				sock.sen
+				//boost::asio::socket_base::broadcast option(true);
+				//socket.set_option(option);
+
+				//std::string ip;
+				//std::set<unsigned int> result;
+				//getlocalip(result,ip,io_service);//得到主机ip地址
+
+				//ipinfo->count=1;
+				//ipinfo->iplist[0].ipaddr=ip;
+
+				//udp::endpoint destination(boost::asio::ip::address::from_string("255.255.255.255"), PORT);
+
+				//socket.send_to(boost::asio::buffer(ipinfo,(sizeof(_ipinfo)+sizeof(_ipinfo().iplist[0]))),destination);
 			}
 		}
 
