a14c93a069f77eb8603999e671f7c0e59de20211
cedricporter@gmail.com <cedricporter@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-10-28 10:35:34 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@395 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/Scar/Scar/IClient.h
+++ Code/Scar/Scar/IClient.h
@@ -1,76 +1,76 @@
-/********************************************************************
-创建时间: 2011:9:27   14:53
-文件名:   IClient.h
-作者:     华亮 Cedric Porter [ Stupid ET ]	
-说明:     客户端接口。客户端端应该继承IClient并实现IClient的接口，
-		系统其他部分针对IClient接口编程  
-
-*********************************************************************/
-
-#ifndef _ICLIENT_H_
-#define _ICLIENT_H_
-
-#include <functional>
-#include <string>
-#include <list>
-
-namespace Network
-{
-	/*
-	** 名字：ServerInfo
-	** 说明：由IClient返回的当前局域网服务器的信息
-	**
-	*/
-	struct ServerInfo
-	{
-		std::wstring ip;
-		std::wstring roomName;
-		int			maxPlayerNumber;
-		int			currentPlayerNumber;
-	};
-
-	typedef std::function< void*( void* ) > IClientOnMsgCallbackFuncType;
-	typedef std::list< ServerInfo > ServerInfoList;
-
-	/*
-	** 名字：IClient
-	** 说明：网络客户端接口
-	**
-	*/
-	class IClient
-	{
-	public:
-		virtual ~IClient() {}
-
-		//************************************
-		// 函数名:  IClient::SearchGameRoom
-		// 返回值:  ServerInfoList
-		// 函数修饰:
-		// 描述：   查找当前局域网的服务器，我们先暂定端口号为 54321
-		//************************************
-		virtual ServerInfoList SearchGameRoom() = 0;
-
-		//************************************
-		// 函数名:  IClient::EnterGameRoom
-		// 返回值:  int
-		// 参数:    const std::wstring & ip
-		// 函数修饰:
-		// 描述：   加入服务器
-		//************************************
-		virtual int EnterGameRoom( const std::wstring& ip ) = 0;
-
-		//************************************
-		// 函数名:  IClient::SetOnMsgCallbackFunc
-		// 返回值:  void
-		// 参数:    IClientOnMsgCallbackFuncType func
-		// 函数修饰:
-		// 描述：   设置消息到达时的回调处理函数，每次在客户端消息到达时，客户端都应调用
-		//			此函数设置的回调函数来处理消息。
-		//************************************
-		virtual void SetOnMsgCallbackFunc( IClientOnMsgCallbackFuncType func ) = 0;
-	};
-
-}
-
-
-#endif
+///********************************************************************
+//创建时间: 2011:9:27   14:53
+//文件名:   IClient.h
+//作者:     华亮 Cedric Porter [ Stupid ET ]	
+//说明:     客户端接口。客户端端应该继承IClient并实现IClient的接口，
+//		系统其他部分针对IClient接口编程  
+//
+//*********************************************************************/
+//
+//#ifndef _ICLIENT_H_
+//#define _ICLIENT_H_
+//
+//#include <functional>
+//#include <string>
+//#include <list>
+//
+//namespace Network
+//{
+//	/*
+//	** 名字：ServerInfo
+//	** 说明：由IClient返回的当前局域网服务器的信息
+//	**
+//	*/
+//	struct ServerInfo
+//	{
+//		std::wstring ip;
+//		std::wstring roomName;
+//		int			maxPlayerNumber;
+//		int			currentPlayerNumber;
+//	};
+//
+//	typedef std::function< void*( void* ) > IClientOnMsgCallbackFuncType;
+//	typedef std::list< ServerInfo > ServerInfoList;
+//
+//	/*
+//	** 名字：IClient
+//	** 说明：网络客户端接口
+//	**
+//	*/
+//	class IClient
+//	{
+//	public:
+//		virtual ~IClient() {}
+//
+//		//************************************
+//		// 函数名:  IClient::SearchGameRoom
+//		// 返回值:  ServerInfoList
+//		// 函数修饰:
+//		// 描述：   查找当前局域网的服务器，我们先暂定端口号为 54321
+//		//************************************
+//		virtual ServerInfoList SearchGameRoom() = 0;
+//
+//		//************************************
+//		// 函数名:  IClient::EnterGameRoom
+//		// 返回值:  int
+//		// 参数:    const std::wstring & ip
+//		// 函数修饰:
+//		// 描述：   加入服务器
+//		//************************************
+//		virtual int EnterGameRoom( const std::wstring& ip ) = 0;
+//
+//		//************************************
+//		// 函数名:  IClient::SetOnMsgCallbackFunc
+//		// 返回值:  void
+//		// 参数:    IClientOnMsgCallbackFuncType func
+//		// 函数修饰:
+//		// 描述：   设置消息到达时的回调处理函数，每次在客户端消息到达时，客户端都应调用
+//		//			此函数设置的回调函数来处理消息。
+//		//************************************
+//		virtual void SetOnMsgCallbackFunc( IClientOnMsgCallbackFuncType func ) = 0;
+//	};
+//
+//}
+//
+//
+//#endif
--- Code/Scar/Scar/IServer.h
+++ Code/Scar/Scar/IServer.h
@@ -1,64 +1,64 @@
-/********************************************************************
-创建时间: 2011-9-27   15:00
-文件名:   IServer.h
-作者:     华亮 Cedric Porter [ Stupid ET ]	
-说明:     服务器接口。服务器端应该继承IServer并实现IServer的接口，
-		系统其他部分针对IServer接口编程
-
-*********************************************************************/
-
-#ifndef _ISERVER_H
-#define _ISERVER_H
-
-#include <string>
-
-namespace Network
-{
-
-	/*
-	** 名字：IServer
-	** 说明：服务器接口
-	**
-	*/
-	class IServer
-	{
-	public:
-		virtual ~IServer() {}
-
-		//************************************
-		// 函数名:  Network::IServer::CreateGameRoom
-		// 返回值:  int
-		// 参数:    int maxPlayerNumber					最大玩家数量
-		// 参数:    const std::wstring & gameRoomName		游戏房间的名字
-		// 函数修饰:
-		// 描述：   创建游戏房间
-		//************************************
-		virtual int CreateGameRoom( 
-			int maxPlayerNumber, 
-			const std::wstring& gameRoomName 
-			) = 0;
-
-		//************************************
-		// 函数名:  Network::IServer::Start
-		// 返回值:  int
-		// 函数修饰:
-		// 描述：   启动游戏房间，开始接受客户端连接
-		//************************************
-		virtual int Start() = 0;
-
-		//************************************
-		// 函数名:  Network::IServer::Stop
-		// 返回值:  int
-		// 函数修饰:
-		// 描述：   停止游戏房间
-		//************************************
-		virtual int Stop() = 0;
-
-	};
-
-
-}
-
-
-
-#endif
+///********************************************************************
+//创建时间: 2011-9-27   15:00
+//文件名:   IServer.h
+//作者:     华亮 Cedric Porter [ Stupid ET ]	
+//说明:     服务器接口。服务器端应该继承IServer并实现IServer的接口，
+//		系统其他部分针对IServer接口编程
+//
+//*********************************************************************/
+//
+//#ifndef _ISERVER_H
+//#define _ISERVER_H
+//
+//#include <string>
+//
+//namespace Network
+//{
+//
+//	/*
+//	** 名字：IServer
+//	** 说明：服务器接口
+//	**
+//	*/
+//	class IServer
+//	{
+//	public:
+//		virtual ~IServer() {}
+//
+//		//************************************
+//		// 函数名:  Network::IServer::CreateGameRoom
+//		// 返回值:  int
+//		// 参数:    int maxPlayerNumber					最大玩家数量
+//		// 参数:    const std::wstring & gameRoomName		游戏房间的名字
+//		// 函数修饰:
+//		// 描述：   创建游戏房间
+//		//************************************
+//		virtual int CreateGameRoom( 
+//			int maxPlayerNumber, 
+//			const std::wstring& gameRoomName 
+//			) = 0;
+//
+//		//************************************
+//		// 函数名:  Network::IServer::Start
+//		// 返回值:  int
+//		// 函数修饰:
+//		// 描述：   启动游戏房间，开始接受客户端连接
+//		//************************************
+//		virtual int Start() = 0;
+//
+//		//************************************
+//		// 函数名:  Network::IServer::Stop
+//		// 返回值:  int
+//		// 函数修饰:
+//		// 描述：   停止游戏房间
+//		//************************************
+//		virtual int Stop() = 0;
+//
+//	};
+//
+//
+//}
+//
+//
+//
+//#endif
