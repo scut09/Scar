7249f878c1ad721284f6663fce53ac2f9e0fc290
cedricporter@gmail.com <cedricporter@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-11-09 19:39:08 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@537 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- bin/Python/ScenesCreator.py
+++ bin/Python/ScenesCreator.py
@@ -10,39 +10,13 @@ ScenesDict = dict()
 
 def CreateGameScenes():
     '由C++调用来初始化场景结构'
-    mainMenu = MenuScene( "MainMenuIni" )
-    SaveScenes( "mainMenu", mainMenu )
+    sceneMgr = GetGameSceneManager()
+    mainMenu = sceneMgr.AddMenuScene( "MainMenuIni" )
+    mainMenu.SetName( "MainMenuIni" )
 
-    multiMenu = MenuScene( "MultiMenuIni" )
-    SaveScenes( "multiMenu", multiMenu )
-
-    #multiPrepare = MenuScene( "MultiPrepare" )
-    #SaveScenes( "multiPrepare", multiPrepare
-    
-    start = StartScene()
-    SaveScenes( "start", start )
-    
-    multi = MultiplayerScene()
-    SaveScenes( "m", multi )
-    
-    start.Scenes[ 0 ] = multi
+    multiMenu = sceneMgr.AddMenuScene( "MultiMenuIni" )
+    multiMenu.SetName( "MultiMenuIni" )
+   
+    multi = sceneMgr.AddMultiplayerScene()    
     
     ChangeGameScene( multi )
-
-def GetScene( name ):
-    '输入一个场景的名字，返回一个场景对象'
-    global ScenesDict
-    return ScenesDict[ name ]
-
-def DeleteScenes():
-    '回收场景资源'
-    global ScenesDict
-    ScenesDict = dict()
-
-def SaveScenes( name, scene ):
-    '''
-    保存整个场景跳转状态图
-    这里保存一个场景的名字和它的实例的映射
-    '''
-    global ScenesDict
-    ScenesDict[ name ] = scene
