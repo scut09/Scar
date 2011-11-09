cff99d343f0186befbfae14a993a2528c5d3c8dc
cedricporter@gmail.com <cedricporter@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-11-09 10:42:45 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@519 81c3a017-244d-672b-1dc6-c8d6962b81c2

---
+++ Code/Scar/Scar/GameSceneManager.h
@@ -0,0 +1,29 @@
+/********************************************************************
+    创建时间: 2011-11-9   16:07
+    文件名:   GameSceneManager.h
+    作者:     华亮 Cedric Porter [ Stupid ET ]	
+    说明:     场景管理类
+
+*********************************************************************/
+
+#ifndef GameSceneManager_h__
+#define GameSceneManager_h__
+
+#include "GameScene.h"
+#include "MultiplayerScene.h"
+#include "MenuScene.h"
+
+
+class GameSceneManager
+{
+public:
+	GameScene*	CurrentGameScene;
+
+	GameScene* AddGameScene()
+	{
+
+	}
+
+};
+
+#endif // GameSceneManager_h__
