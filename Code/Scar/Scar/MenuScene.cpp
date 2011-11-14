d3096cb9f4dbb19a420361ad6b8b7c6beb39f8fd
xie.jf2011@gmail.com <xie.jf2011@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-11-14 16:41:49 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@563 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- Code/Scar/Scar/MenuScene.cpp
+++ Code/Scar/Scar/MenuScene.cpp
@@ -156,12 +156,45 @@ using namespace scene;
 
 	if ( SceneName == "MultiMenuIni")
 	{
-		/*IUIObject* root1 = pEngine->GetUIManager()->GetRootUIObject();
-		IUIObject * house = pEngine->GetUIManager()->AddUIBox(root1, 400, 400 );
-		house->SetPosition( vector2df( 800, 400 ) );
-		house->LoadImage( "../media/UIResource/Menu/scrollbar_hbackground.png" );
-		((CUIBox*)house)->test( 10 );*/
-
+		IUIObject* root1 = pEngine->GetUIManager()->GetRootUIObject();
+			IUIObject* house = pEngine->GetUIManager()->AddUIImage( 0, 200, 200 );
+			 house->SetPosition(vector2df( 800, 400) );
+			 house->LoadImage("../media/UIResource/Menu/scrollbar_hbackground.png");
+			IUIObject* text1 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"主机", SColor( 255,255,255,255));
+			IUIObject* text2 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"疤痕", SColor( 255,255,255,255));
+			IUIObject* text3 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"地图", SColor( 255,255,255,255));
+			IUIObject* text4 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"宇宙", SColor( 255,255,255,255));
+			IUIObject* text5 =pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"人数", SColor( 255,255,255,255));
+			IUIObject* text6 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40,L"100", SColor( 255,255,255,255));
+			IUIObject* text7 =pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"最大人数", SColor( 255,255,255,255));
+			IUIObject* text8 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"200", SColor( 255,255,255,255));
+			IUIObject* text9 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"延迟", SColor( 255,255,255,255));
+			IUIObject* text10 = pEngine->GetUIManager()->AddUIStaticText( 0, 20, 40, L"0", SColor( 255,255,255,255));
+			f32 PosX = -50;
+			f32 PosY = -50;
+			f32 Base = 80;
+			text1->SetPosition( vector2df( PosX, PosY));
+			house->AddChild( text1 );
+			text2->SetPosition( vector2df( PosX + Base, PosY ));
+			house->AddChild( text2 );
+			text3->SetPosition( vector2df( PosX, PosY + 30));
+		    house->AddChild( text3 );
+			text4->SetPosition( vector2df ( PosX + Base, PosY + 30));
+			house->AddChild( text4 );
+			text5->SetPosition( vector2df( PosX, PosY + 60));
+			house->AddChild( text5 );
+			text6->SetPosition( vector2df( PosX + Base , PosY + 60));
+			house->AddChild( text6 );
+			text7->SetPosition( vector2df( PosX, PosY + 90 ));
+			house->AddChild( text7 );
+			text8->SetPosition( vector2df( PosX + Base ,PosY + 90));
+			house->AddChild( text8 );
+			text9->SetPosition( vector2df( PosX, PosY + 120));
+			house->AddChild( text9 );
+			text10->SetPosition( vector2df( PosX +  Base, PosY + 120));
+			house->AddChild( text10 );
+		/*((CUIBox*)house)->test( 10 );*/
+		
 		//char buffer[255];
 		//stringw str;
 		//root = pEngine->GetUIManager()->GetRootUIObject();
