2c31bce3d124edc27148bfaaa53624662dbcdd9c
kid.shusang@gmail.com <kid.shusang@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-11-16 11:05:16 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@575 81c3a017-244d-672b-1dc6-c8d6962b81c2

--- bin/Python/MultiPlayCallBacks.py
+++ bin/Python/MultiPlayCallBacks.py
@@ -4,9 +4,31 @@
 #                                               #
 #################################################
 
+    # 按钮通用
+def GeneralDefault( node ):
+    node.GetChildren()[0].SetVisible( True )
+    node.GetChildren()[1].SetVisible( False )
+def GeneralOver( node ):
+    node.GetChildren()[0].SetVisible( False )
+    node.GetChildren()[1].SetVisible( True )
+
     # 选择阵营菜单  加达里盖伦特通用
 def SelectCGDefault( node ):
     node.GetChildren()[0].SetVisible( False )
 def SelectCGOver( node ):
     node.GetChildren()[0].SetVisible( True )
 
+    # 选择阵营菜单  选择加达里
+def SelectCClick( node ):
+    pass
+    # 选择阵营菜单  选择盖伦特
+def SelectGClick( node ):
+    pass
+
+    '''# 选择舰船菜单 通用
+def ShipMenuButtonDefault( node ):
+    node.GetChildren()[0].SetVisible( True )
+    node.GetChildren()[1].SetVisible( False )
+def ShipMenuButtonOver( node ):
+    node.GetChildren()[0].SetVisible( False )
+    node.GetChildren()[1].SetVisible( True )'''
