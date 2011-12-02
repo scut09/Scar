a9be12d4a3309dd10eda41f92b1d217150ba76b1
yangxuyu.fish@gmail.com <yangxuyu.fish@gmail.com@81c3a017-244d-672b-1dc6-c8d6962b81c2>
2011-12-02 11:23:07 +0000

    git-svn-id: https://opengl-scut.googlecode.com/svn/trunk@650 81c3a017-244d-672b-1dc6-c8d6962b81c2

---
+++ Code/Scar/Scar/BillBoardExplosionAnimator.h
@@ -0,0 +1,73 @@
+#ifndef BillBoardExplosionAnimator_h__
+#define BillBoardExplosionAnimator_h__
+
+
+#include "ISceneNode.h"
+#include "MyIrrlichtEngine.h"
+#include "Toolkit.h"
+using namespace irr;
+
+class BillBoardExplosionAnimator : public ISceneNodeAnimator
+{
+
+
+public:
+	BillBoardExplosionAnimator(f32 d) : ChangeRate(1.0f), State(0), IsFirst(true)
+	{
+		MaxSize.Width = MaxSize.Height = d * 5;
+	}
+
+	virtual void animateNode( ISceneNode* node, u32 timeMs ) 
+	{
+		if (IsFirst)
+		{
+			OriginalSize = ((IBillboardSceneNode*)node)->getSize();
+			MaxRate =  MaxSize.Width / OriginalSize.Width;
+			IsFirst = false;
+		}
+		f32 increasestep, discreasestep;
+		increasestep = MaxRate / 200;
+		discreasestep = MaxRate / 150;
+
+		dimension2df size;
+		switch( State )
+		{
+		case 0:
+			size = OriginalSize * ChangeRate;
+			ChangeRate += increasestep;
+			if ( ChangeRate > MaxRate )
+			{
+				State = 1;
+			}
+			break;
+		case 1:
+			size = OriginalSize * ChangeRate;
+			ChangeRate -= discreasestep;
+			if ( ChangeRate < 1.50f )
+			{
+				((IBillboardSceneNode*)node)->setSize(dimension2df(0.f, 0.f));
+				return;
+			}
+			break;
+		}
+
+
+		((IBillboardSceneNode*)node)->setSize(size);
+
+
+	}
+
+	virtual ISceneNodeAnimator* createClone( ISceneNode* node, ISceneManager* newManager=0 ) 
+	{
+		throw std::exception("The method or operation is not implemented.");
+	}
+private:
+	f32 ChangeRate;
+	f32 MaxRate;
+	s32 State;
+	dimension2df OriginalSize;
+	bool IsFirst;
+	dimension2df MaxSize;
+};
+
+#endif // BillBoardExplosionAnimator_h__
---
+++ Code/Scar/Scar/ExplosionMyself.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "SceneNodeShader.h"
+#include "MyIrrlichtEngine.h"
+#include "BillBoardExplosionAnimator.h"
+
+
+class ExplosionMyself
+{
+public:
+	ExplosionMyself(ISceneNode* pN) : m_pNode(pN) 
+	{
+		m_pSmgr = MyIrrlichtEngine::GetEngine()->GetSceneManager();
+		m_pDriver = MyIrrlichtEngine::GetEngine()->GetVideoDriver();
+		m_pExplosiongBillBoard = m_pSmgr->addBillboardSceneNode(m_pNode);
+
+		m_pExplosiongBillBoard->setMaterialTexture(0, m_pDriver->getTexture("../media/Space/sun2.png"));
+		m_pExplosiongBillBoard->setMaterialType(EMT_TRANSPARENT_ADD_COLOR);
+		m_pExplosiongBillBoard->setMaterialFlag(EMF_LIGHTING, false);
+		
+		m_pExplosiongBillBoard->setSize(dimension2df(100.f, 100.f));
+	}
+
+	void Explode()
+	{
+		vector3df v[8];
+		vector3df center;
+		center = m_pNode->getBoundingBox().getCenter();
+		m_pNode->getBoundingBox().getEdges(v);
+		f32 MaxDis = 0.0f;
+
+		for (int i = 0; i < 8; i++)
+		{
+			MaxDis = v->getDistanceFrom(center) > MaxDis ? v->getDistanceFrom(center) : MaxDis;
+		}
+
+		MaxDis *= 2;
+
+		m_pNode->setScale(vector3df(0));
+		BillBoardExplosionAnimator* anim = new BillBoardExplosionAnimator(MaxDis);
+		m_pExplosiongBillBoard->addAnimator( anim );
+		anim->drop();
+	}
+
+	void EndExplode()
+	{
+		m_pNode->setScale(vector3df(1.0f));
+	}
+
+private:
+	ISceneNode* m_pNode;
+	IBillboardSceneNode* m_pExplosiongBillBoard;
+	ISceneManager* m_pSmgr;
+	IVideoDriver* m_pDriver;
+};
